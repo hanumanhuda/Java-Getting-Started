fc3be856a9997a8506f9d7d47595d65a8177b9d0
Hanuman Huda <hanumanramhuda96@gmail.com>
2019-04-10 01:09:03 +0530

    why DI and added libs of Junit

--- src/hhuda/com/DIDemo.java
+++ src/hhuda/com/DIDemo.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+package hhuda.com;
+
+import org.junit.Test;
+
+import java.awt.*;
 
 public class DIDemo {
+
+    TextField textField;
+    public DIDemo(TextField textField){
+        this.textField = textField;
+    }
+    //this method is not testable because Dependencies are calling to real method so all
+    //dependencies must execute to run this Test
+    //this issue can be resolved by factory pattern or DI
+    public void postButtonClicked(){
+            String text = textField.getText();
+            if (text.length() > 140){
+                Shortener  shortener =  new TinyUrlShortener();
+                text =shortener.shorten(text);
+            }
+            if( text.length() < 140 ){
+                Tweeter tweeter = new SMSTweeter();
+                tweeter.send(text);
+            }
+    }
+}
+
+interface Tweeter{
+    void send(String text);
+}
+
+class SMSTweeter implements Tweeter{
+
+    @Override
+    public void send(String text) {
+        System.out.println("SMSTweeter.send(): SMS Tweeter send the text as: "+text);
+    }
+}
+
+
+interface Shortener{
+    String shorten(String text);
+}
+
+
+class TinyUrlShortener implements Shortener{
+
+    @Override
+    public String shorten(String text) {
+        System.out.println("TinyUrlShortener.shorten()");
+        return text.substring(0,text.length()>140 ?140 : text.length());
+    }
 }
--- src/hhuda/com/DIDemoTest.java
+++ src/hhuda/com/DIDemoTest.java
@@ -1,5 +1,27 @@
+package hhuda.com;
+
+import org.junit.Test;
+
+import java.awt.*;
+
 import static org.junit.Assert.*;
 
 public class DIDemoTest {
 
+    @Test
+    public void postButtonClicked() {
+        System.out.println("Tested Started");
+        TextField textField =new TextField();
+        textField.setText("Button Text is test");
+        DIDemo diDemo =new DIDemo(textField);
+        diDemo.postButtonClicked();
+    }
+
+    // Write a main method only if:
+    //     - you don't want to give the runner in the command line and
+    //     - you want your class to be self-executable.
+    public static void main(String[] args) {
+        System.out.println("Test Main Started");
+        //org.junit.runner.JUnitCore.main("hhuda.com.DIDemoTest");
+    }
 }
